3b794c767735a5c1ed585dda714efa7bbb509a87
Chao Li <lichao@microsoft.com>
2023-10-12 15:23:36 -0700

    first commit

---
+++ ds/Tree/binaryTree.py
@@ -0,0 +1,17 @@
+class Node:
+    def __init__():
+        pass
+
+class BinaryTree:
+    def __init__(self) -> None:
+        pass
+
+    def insert(self, key):
+        pass
+
+    def delete(self, key, value):
+        pass
+
+bt = BinaryTree()
+bt.insert("1")
+bt.delete("1")
