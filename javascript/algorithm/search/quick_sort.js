7fa04432dc963545810fe31296d8eb4e422f2a7d
Chao Li <lichao@microsoft.com>
2023-10-15 22:32:54 -0700

    Add all

--- javascript/algorithm/search/quick_sort.js
+++ javascript/algorithm/search/quick_sort.js
@@ -1,22 +1,20 @@
-def quicksort(arr, leftmost, rightmost):
-    if leftmost < rightmost:
-        pivot = partition(arr, leftmost, rightmost)
-        quicksort(arr, leftmost, pivot-1)
-        quicksort(arr, pivot+1, rightmost)
-
-def partition(arr, leftmost, rightmost):
-    pivot = arr[rightmost]
-    i = leftmost-1
-    for j in range(leftmost, rightmost):
-        if arr[j] < pivot:
-            i += 1
-            arr[i], arr[j] = arr[j], arr[i]
-    arr[i+1], arr[rightmost] = arr[rightmost], arr[i+1]
-    return i+1
-
-arr = [64, 34, 25, 12, 22, 11, 90]
-quicksort(arr, 0, len(arr)-1)
-
-print("Sorted array is:")
-for i in range(len(arr)):
-    print("%d" %arr[i]),
+const quicksort = (arr) => {
+    let swapped;
+  
+    do {
+      swapped = false;
+      for (let i = 0; i < arr.length - 1; i++) {
+        if (arr[i] > arr[i + 1]) {
+          let temp = arr[i];
+          arr[i] = arr[i + 1];
+          arr[i + 1] = temp;
+          swapped = true;
+        }
+      }
+    } while (swapped);
+  
+    return arr;
+  };
+  
+  let myArray = [12, 10, 3, 7, 4];
+  console.log(quicksort(myArray)); // returns [3, 4, 7, 10, 12]
