c8228aa31d936d49a4fce6b1b95ac496452d0097
Chao Li <lichao@microsoft.com>
2023-10-26 16:01:32 -0700

    Add all

---
+++ algorithm/python/ds/Array/1326.Minimum Number of Taps to Open to Water a Garden.py
@@ -0,0 +1,22 @@
+# https://leetcode.com/problems/minimum-number-of-taps-to-open-to-water-a-garden/
+
+def minTaps(n, ranges):
+    memo = {}
+    def recur(ind):
+        if ind <= 0: return 0
+        if ind in memo: return memo[ind]
+
+        count = float('inf')
+        for i in range(ind, -1, -1):
+            if ranges[i] > 0 and ranges[i] >= ind - i:
+                count = min(count, recur(i-ranges[i]) + 1)
+        memo[ind] = count
+        return memo[ind]
+    
+    ans = recur(n)
+    return -1 if ans >= float('inf') else ans
+
+# positive 
+print(minTaps(5, [3,4,1,1,0,0]))
+
+print(minTaps(3, [0,0,0,0]))
--- algorithm/python/ds/Array/54.spiral matrix.py
+++ algorithm/python/ds/Array/54.spiral matrix.py
@@ -34,5 +34,4 @@ def spiralOrder(matrix):
 # positive 
 print(spiralOrder([[1,2,3],[4,5,6],[7,8,9]]))
 
-# negative
 print(spiralOrder([[1,2,3,4],[5,6,7,8],[9,10,11,12]]))
